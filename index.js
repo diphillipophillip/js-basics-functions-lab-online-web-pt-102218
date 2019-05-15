0c2c67ea6edf55b14b55954acd5b93c06060401f
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-15 14:21:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function distanceFromHqInFeet (distance) {
   
   function calculatesFarePrice (start, destination) {
     const distance = distanceTravelledInFeet(starting, ending);
-    if (distance < 400) {
+    if (distance <= 400) {
     return 0 
     }
   }
