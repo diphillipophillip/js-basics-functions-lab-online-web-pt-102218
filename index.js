1dda5083a31df85f0de007ab2224990c7c12489d
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-15 14:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function distanceFromHqInFeet (distance) {
     if (distance <= 400) {
     return 0 
     } else if (distance >= 401 && distance <= 2000 ) {
-      return (distance * 2)
+      return .02
     }
   }
