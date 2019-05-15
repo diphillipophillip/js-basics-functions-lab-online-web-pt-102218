cb94108fe61674115bfe5ecfacf41ca7f56914d0
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-15 14:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ function distanceFromHqInFeet (distance) {
   }
   
   function calculatesFarePrice (start, destination) {
-    if (((starting - ending) * 264) <= 400) {
-      return 0
-    }
+      return ((starting - ending) * 264)
+    
   }
